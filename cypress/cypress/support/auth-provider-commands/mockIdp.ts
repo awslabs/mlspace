7854b7cdcf531f2e88d7719320ebc43cf64ba8cf
Stephen Clement <clemsa@amazon.com>
2024-04-05 18:42:49 -0400

    Enables Cypress To Work With Old MockIdp Framework

--- cypress/cypress/support/auth-provider-commands/mockIdp.ts
+++ cypress/cypress/support/auth-provider-commands/mockIdp.ts
@@ -15,43 +15,42 @@ const loginToMockIdP = (baseUrl: string, username: string, password: string) => 
         () => {
             cy.visit(baseUrl);
             cy.request(baseUrl + '/env.js').then((resp) => {
-                const OIDC_URL_REGEX = /["']*OIDC_URL['"]*:\s*['"]([A-Za-z:\-./0-9]+)['"]/;
+                const OIDC_URL_REGEX = /["']OIDC_URL['"]:\s*['"]([A-Za-z:\-./0-9]+)['"]/;
                 const matches = OIDC_URL_REGEX.exec(resp.body);
                 if (matches && matches.length === 2) {
                     idpOrigin = matches[1];
                 }
-                const getTopLevelDomain = (url: string): string => {
-                    const parts = url.split('.');
-                    return parts[parts.length - 2];
-                };
-                if (getTopLevelDomain(baseUrl) === getTopLevelDomain(idpOrigin)) {
-                    cy.get('#username').type(username);
-                    cy.get('#password').type(password, {
-                        // use log: false to prevent your password from showing in the Command Log
-                        log: false,
-                    });
+                // const getTopLevelDomain = (url: string): string => {
+                //     const parts = url.split('.');
+                //     return parts[parts.length - 2];
+                // };
+                // if (getTopLevelDomain(baseUrl) === getTopLevelDomain(idpOrigin)) {
+                //     cy.get('#username').type(username);
+                //     cy.get('#password').type(password, {
+                //         // use log: false to prevent your password from showing in the Command Log
+                //         log: false,
+                //     });
 
-                    cy.get('#btnLogin').click();
-                } else {
-                    cy.origin(
-                        idpOrigin,
-                        {
-                            args: {
-                                username,
-                                password,
-                            },
+                //     cy.get('#btnLogin').click();
+                // } else {
+                cy.origin(
+                    idpOrigin,
+                    {
+                        args: {
+                            username,
+                            password,
                         },
-                        ({ username, password }) => {
-                            cy.get('#username').type(username);
-                            cy.get('#password').type(password, {
-                                // use log: false to prevent your password from showing in the Command Log
-                                log: false,
-                            });
+                    },
+                    ({ username, password }) => {
+                        cy.get('#username').type(username);
+                        cy.get('#password').type(password, {
+                            // use log: false to prevent your password from showing in the Command Log
+                            log: false,
+                        });
 
-                            cy.get('#btnLogin').click();
-                        }
-                    );
-                }
+                        cy.get('#btnLogin').click();
+                    }
+                );
                 cy.wait(2000);
                 cy.url().should('equal', baseUrl);
                 cy.clearAllCookies();
@@ -70,6 +69,71 @@ const loginToMockIdP = (baseUrl: string, username: string, password: string) => 
     log.snapshot('after');
     log.end();
 };
+
+const loginToCognito = (baseUrl: string, username: string, password: string) => {
+    const log = Cypress.log({
+        displayName: 'Mock IdP Login',
+        message: [`🔐 Authenticating | ${username}`],
+        autoEnd: false,
+    });
+    let authOrigin = '';
+    log.snapshot('before');
+    // Temporarily suppress exceptions. We expect to get 401's which will trigger the login redirect
+    cy.on('uncaught:exception', () => {
+        return false;
+    });
+    cy.session(
+        `cognito-${username}`,
+        () => {
+            // Handle cognito portal information
+            cy.request(baseUrl + '/env.js').then((resp) => {
+                const OIDC_URL_REGEX = /["']OIDC_URL['"]:\s*['"]([A-Za-z:\-._/0-9]+)['"]/;
+                const matches = OIDC_URL_REGEX.exec(resp.body);
+                if (matches && matches.length === 2) {
+                    authOrigin = matches[1];
+                }
+                const getTopLevelDomain = (url: string): string => {
+                    const parts = url.split('.');
+                    return parts[parts.length - 2];
+                };
+                // click the login link
+                cy.visit(baseUrl);
+                cy.contains('button', 'Login').click();
+                //authOrigin = cy.location('origin');
+                cy.origin(
+                    authOrigin,
+                    {
+                        args: {
+                            username,
+                            password,
+                        },
+                    },
+                    ({ username, password }) => {
+                        cy.get('#signInFormUsername').type(username);
+                    }
+                );
+                cy.wait(2000);
+                cy.url().should('equal', baseUrl);
+            });
+        },
+        {
+            validate: () => {
+                cy.wrap(sessionStorage)
+                    .invoke('getItem', `oidc.user:${authOrigin}:web-client`)
+                    .should('exist');
+
+            }
+        }
+    );
+    log.snapshot('after');
+    log.end();
+};
+
+
+
+Cypress.Commands.add('loginToCognito', (baseUrl: string, username: string, password: string) => {
+    return loginToCognito(baseUrl, username, password);
+});
 
 
 
