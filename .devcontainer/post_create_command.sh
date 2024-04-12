8d12754f2c72eced89a36681cac9aa026fca5896
Stephen Clement <clemsa@amazon.com>
2024-04-12 03:23:53 +0000

    Revision 3: Dev container functionality

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -15,8 +15,10 @@ cd ..
 
 # Setup cypress
 cd cypress
-npm install
+sudo apt-get update -y
 sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb libgbm-dev
+npm install
+node_modules/.bin/cypress install
 cd ..
 
 pre-commit install
