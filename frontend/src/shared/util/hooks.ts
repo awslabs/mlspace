668cb50339073fcc3608d746ee72b9aa40ae3bd7
Kyle Lilly <KyleLilly@users.noreply.github.com>
2024-05-07 15:42:42 -0400

    Fix bug where intervals were being created everytime a background refresh happened due to component mounting. (#79)

--- frontend/src/shared/util/hooks.ts
+++ frontend/src/shared/util/hooks.ts
@@ -19,8 +19,8 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 /**
  * A custom hook that runs an action periodically in the background to refresh data.
- * 
- * @param {Function} action - The function to run periodically to refresh data 
+ *
+ * @param {Function} action - The function to run periodically to refresh data
  * @param {Array} deps - The dependencies of the action function
  * @param {boolean} condition - A condition that must be true for the action to run
  * @returns {boolean} isBackgroundRefreshing - Whether the background refresh is currently running
@@ -29,19 +29,26 @@ export function useBackgroundRefresh (action: () => void, deps: readonly unknown
 
     const [isBackgroundRefreshing, setIsBackgroundRefreshing] = useState(false);
     const callbackAction = useCallback(action, [action, ...deps]);
-    
+
     useEffect(() => {
+        // Only start a periodic refresh if the condition is met
         if (condition) {
             const timerId = setInterval(() => {
+                // Once the condition is no longer met we can stop refreshing
+                if (!condition) {
+                    clearInterval(timerId);
+                    return;
+                }
                 setIsBackgroundRefreshing(true);
                 callbackAction();
             }, (window.env.BACKGROUND_REFRESH_INTERVAL || 60) * 1000);
-            
             return () => {
                 clearInterval(timerId);
             };
         }
-    }, [callbackAction, condition]);
+        // We only want to recreate the interval if the condition changes
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [condition]);
     return isBackgroundRefreshing;
 }
 
