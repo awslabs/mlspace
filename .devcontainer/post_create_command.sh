24557ccd9ac86cd7bbdcfbe1b3afd1629f33188d
Stephen Clement <clemsa@amazon.com>
2024-05-15 18:52:34 +0000

    Fixing ACT Tests

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd frontend/ && npm install
+cd frontend/
+sudo apt-get update -y
+sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb libgbm-dev
+npm install
+node_modules/.bin/cypress install
 cd ..
 
 npm install
@@ -11,14 +15,6 @@ cd backend
 pip install .
 pip3 install -r requirements.txt
 pip3 install -r test/requirements.txt
-cd ..
-
-# Setup cypress
-cd cypress
-sudo apt-get update -y
-sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb libgbm-dev
-npm install
-node_modules/.bin/cypress install
 cd ..
 
 pre-commit install
