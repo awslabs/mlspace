62a19fb6ff4f47fc42294ebfc14a36a532bc4eae
Douglas O'Meara <odoug@amazon.com>
2024-06-12 17:51:19 -0400

    Rewording

--- lib/constants.ts
+++ lib/constants.ts
@@ -101,15 +101,15 @@ export const COMMON_LAYER_ARN_PARAM = '/mlspace/common-lambda-layer';
 
 // Whether or not translate functionality should be include in the deployment
 /**
- * @deprecated This constant will be removed in the next release. Activating and deactivating the translate service
- * is now handled in the app config feature.
+ * @deprecated This constant will be removed in the next release. Activating and deactivating the Translate service
+ * is now handled by the app config feature.
  */
 export const ENABLE_TRANSLATE = true;
 
 // Whether or not GroundTruth labeling functionality should be include in the deployment
 /**
  * @deprecated This constant will be removed in the next release. Activating and deactivating the GroundTruth service
- * is now handled in the app config feature.
+ * is now handled by the app config feature.
  */
 export const ENABLE_GROUNDTRUTH = true;
 
