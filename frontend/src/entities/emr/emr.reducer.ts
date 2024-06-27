c245250c5a4dec5b6882f52d9a9a51c3b287682c
Douglas O'Meara <odoug@amazon.com>
2024-06-27 17:28:50 -0400

    EMR detail page race condition fix

--- frontend/src/entities/emr/emr.reducer.ts
+++ frontend/src/entities/emr/emr.reducer.ts
@@ -15,7 +15,7 @@
 */
 
 import axios from '../../shared/util/axios-utils';
-import { createSlice, createAsyncThunk, isFulfilled, isPending } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, isFulfilled, isPending, isRejected } from '@reduxjs/toolkit';
 import { EMRCluster } from './emr.model';
 import { PagedResponsePayload, ServerRequestProps } from '../../shared/util/table-utils';
 import { addPagingParams } from '../../shared/util/url-utils';
@@ -143,6 +143,13 @@ export const EMRClusterSlice = createSlice({
                 return {
                     ...state,
                     loadingCluster: true,
+                };
+            })
+            .addMatcher(isRejected(getEMRCluster), (state) => {
+                return {
+                    ...state,
+                    loadingCluster: true, // Continue loading until a call goes through
+                    cluster: {} as EMRCluster,
                 };
             })
             .addMatcher(isPending(listEMRClusters), (state) => {
